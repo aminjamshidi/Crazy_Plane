f2b9672ec0e177e21f79061d69543195bd9fcdcd
mohamad amin jamshidi <mohamadaminjamshidi73@gmail.com>
2023-09-30 20:15:53 +0330

    beta version

---
+++ Assets/CODE&SET/moveing_obstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveing_obstacle : MonoBehaviour {
+
+	// Use this for initialization
+    [SerializeField]Vector3 vector_for_move;
+    [Range(0,1)]
+    [SerializeField]
+    float muserofmoving;
+    Vector3 keep_position;
+    [SerializeField]float period=2f;
+    void Start () {
+        keep_position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (period <=Mathf.Epsilon) { return; }
+        float taw = Mathf.PI * 2f;
+        float cycle = Time.time / period;
+        float racycle = Mathf.Sin(cycle * taw);
+        muserofmoving = racycle / 2f + 0.5f;
+        Vector3 offset = vector_for_move * muserofmoving;
+        transform.position = keep_position + offset;
+	}
+}
---
+++ Assets/CODE&SET/rocket.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rocket : MonoBehaviour {
+    enum State{alive,dead,traspot};
+    State state = State.alive;
+    Rigidbody MOVE;
+    [SerializeField]float fuel = 100;
+    AudioSource voice;
+    bool collsiondisablity=false;
+    [SerializeField]float powerofmotor=100f;
+    [SerializeField]float powerofrotate=100f;
+    [SerializeField]AudioClip audio;
+    [SerializeField] ParticleSystem win;
+    [SerializeField] ParticleSystem deaf;
+    [SerializeField]ParticleSystem fly;
+	// Use this for initialization
+	void Start () {
+        MOVE = GetComponent<Rigidbody>();
+        voice = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (state == State.alive)
+        {
+            if (fuel > 0)
+            {
+                functioning();
+            }
+        }
+        if (Debug.isDebugBuild)
+        {
+            respondTodeBug();
+        }
+    }
+    void respondTodeBug()
+    {
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            state = State.traspot;
+            showscreen();
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            collsiondisablity = !collsiondisablity;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        int level = SceneManager.GetActiveScene().buildIndex;
+        if (state != State.alive||collsiondisablity)
+        {
+            return;
+        }
+        switch (collision.gameObject.tag)
+        {
+            case "resting":
+                state = State.traspot;
+                    win.Play();
+                    Invoke("showscreen", 1f);
+                break;
+            case"dead":
+                fly.Stop();
+                voice.Stop();
+                state = State.dead;
+                deaf.Play();
+                Invoke("showscreen", 1f);
+                break;
+        }    
+    }
+    void showscreen()
+    {
+        int level = SceneManager.GetActiveScene().buildIndex;
+        if (state == State.traspot)
+        {
+            if (level < SceneManager.sceneCountInBuildSettings-1)
+            {
+                level++;
+            }
+            else
+            {
+                level = 0;
+            }
+        }
+        else if (state == State.dead)
+        {
+            level = 0;
+        }
+        SceneManager.LoadScene(level);   
+    }
+     void functioning(){
+            float motormove = powerofmotor * Time.deltaTime;
+        float motorrotate = powerofrotate * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                fuel = fuel - 0.5f;
+                fly.Play();
+                MOVE.AddRelativeForce(Vector3.up * motormove);
+                if (!voice.isPlaying) 
+                { 
+                    voice.PlayOneShot(audio); 
+                }
+            } else {
+                fly.Stop();
+                voice.Stop();
+            }
+            MOVE.freezeRotation = true;
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward * motorrotate);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(-Vector3.forward * motorrotate);
+            }
+            MOVE.freezeRotation = false;
+        }
+      }
+
+
